9b7d6f6f0bb8c3c6f7b2892507f2e973451d0c5a
Chana <chanaa@outlook.co.il>
2019-09-12 23:33:37 +0300

    Final Project- 2

--- app/src/main/java/com/perrchick/dbapplication/MapsActivity.java
+++ app/src/main/java/com/perrchick/dbapplication/MapsActivity.java
@@ -1,5 +1,6 @@
 package com.perrchick.dbapplication;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
 
 import android.content.Intent;
@@ -13,6 +14,15 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -32,6 +42,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public int results;
     public String originalName;
     public int originalResults;
+
+    //our database reference object
+    DatabaseReference databasePlayers;
+    //a list to store all the players from firebase database
+    List<Player> players;
 
 
     public int time;
@@ -57,6 +72,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         time = myIntent.getIntExtra(RecordGame.EXTRA_TIME, 0);
 
+        //getting the reference of artists node
+        databasePlayers = FirebaseDatabase.getInstance().getReference("players");
+        players = new ArrayList<>();
+
         lat = Double.parseDouble(myIntent.getStringExtra(RecordGame.EXTRA_LAT));
         lng = Double.parseDouble(myIntent.getStringExtra(RecordGame.EXTRA_LNG));
 
@@ -65,6 +84,22 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }else {
             location=new LatLng(0,0);
         }
+
+        databasePlayers.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot ds: dataSnapshot.getChildren())
+                {
+                    Player player = ds.getValue(Player.class);
+                        players.add(player);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
     }
 
     /**
@@ -91,6 +126,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 return true;
             }
         });
+        for (int i = 0; i < players.size(); i++) {
+            mMap.addMarker(new MarkerOptions().position(players.get(i).getPlayerLocation()).title("Marker"));
+        }
     }
 
     private void showPlayer() {
